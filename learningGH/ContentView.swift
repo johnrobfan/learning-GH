5119e1e47640d200af52f578756716124f18e783
JR Fan <johnrob.fan@gmail.com>
2021-06-05 16:52:56 -0400

    Initial Commit

---
+++ learningGH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learningGH
+//
+//  Created by JR Fan on 6/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learningGH/learningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningGHApp.swift
+//  learningGH
+//
+//  Created by JR Fan on 6/5/21.
+//
+
+import SwiftUI
+
+@main
+struct learningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
